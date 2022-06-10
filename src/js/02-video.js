44154e9e2d81d2e84abda95a04ff8b1dd92fb4b7
VladyslavLata <vladlatahtc@gmail.com>
2022-06-10 18:42:02 +0300

    2 task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const CURRENT_TIME_VIDEO = "videoplayer-current-time";
+    const iframeEl = document.getElementById('vimeo-player');
+    const player = new Player(iframeEl);
+    const continuePlayingVideo = JSON.parse(localStorage.getItem(CURRENT_TIME_VIDEO));
+
+
+player.on("timeupdate", throttle(onTimeUpdateVideo, 1000));
+    
+if (continuePlayingVideo) {
+  currentTimePlayingVideo();
+}
+
+function onTimeUpdateVideo(e) {
+  console.log(e.seconds);
+  localStorage.setItem(CURRENT_TIME_VIDEO, e.seconds);
+}
+
+function currentTimePlayingVideo() {
+  player.setCurrentTime(continuePlayingVideo);
+}
