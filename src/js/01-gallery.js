81d922fa0f94abd7c857fba196a6b438eede7033
VladyslavLata <vladlatahtc@gmail.com>
2022-06-10 15:19:23 +0300

    1 task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', onPreventDefaultClick);
+
+galleryEl.innerHTML = createGalleryItemMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function onPreventDefaultClick(e) {
+    e.preventDefault();
+  }
+
+function createGalleryItem({ preview, original, description }) {
+  return `<li><a class="gallery__item" href='${original}'>
+  <img class="gallery__image" src='${preview}' 
+  alt='${description}' /></a></li>`;
+}
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems.map(createGalleryItem).join('');
+}
